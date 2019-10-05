223cb696db8ce2bce0ec7da655d1a6ffbce7ad19
Ibcs16 <iagobrayhamcunha@hotmail.com>
2019-10-04 22:26:46 -0300

    inicio mobile arquivos

---
+++ mobile/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/Routes';
+
+export default function App() {
+  return <Routes/>
+}
---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Login() {
+  return <View>
+      <Text>Hello world</Text>
+  </View>
+}
